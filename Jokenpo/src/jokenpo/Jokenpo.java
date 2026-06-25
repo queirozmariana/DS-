5a42aafb817aaf2dd5b0c1df6dc69cc857e74511
Mariana Queiroz <marianapereirampq@gmail.com>
2026-06-25 11:06:42 -0300

    Add files via upload

---
+++ Jokenpo/src/jokenpo/Jokenpo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jokenpo;
+
+/**
+ *
+ * @author maria
+ */
+public class Jokenpo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+
+    java.awt.EventQueue.invokeLater(new Runnable() {
+        public void run() {
+            new TelaJokenpo().setVisible(true);
+        }
+    });
+
+}
+}
