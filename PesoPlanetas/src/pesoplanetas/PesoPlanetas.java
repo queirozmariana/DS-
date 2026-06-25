5df571b9b250da66f15361f3be18f5bab51dcd7f
Mariana Queiroz <marianapereirampq@gmail.com>
2026-06-25 11:06:08 -0300

    Add files via upload

---
+++ PesoPlanetas/src/pesoplanetas/PesoPlanetas.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pesoplanetas;
+
+/**
+ *
+ * @author maria
+ */
+public class PesoPlanetas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+
+    java.awt.EventQueue.invokeLater(new Runnable() {
+        public void run() {
+            new TelaPlanetas().setVisible(true);
+        }
+    });
+
+}}
